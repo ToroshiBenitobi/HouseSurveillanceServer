90574a5e00482277950224023b1b34cab46b2018
Anarhyroch <Anarhyroch@gmail.com>
2021-04-19 11:26:05 +0800

    sensor

--- App/views/sensor.py
+++ App/views/sensor.py
@@ -2,7 +2,7 @@ from flask import Flask, Blueprint, render_template, session, redirect, url_for,
 from App.models import db, Order
 from App.tools.sensorutl import Sensor
 sensorblue = Blueprint('sensorblue', __name__)
-sensor = Sensor()
 @sensorblue.route('/sensor/temperatureinfo', methods=['POST', 'GET'])
 def temperatureinfo():
+    sensor = Sensor()
     return str(sensor.temperature)
