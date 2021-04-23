f89e64c29500e7b526c9d222aead28aff1691a06
Anarhyroch <Anarhyroch@gmail.com>
2021-04-23 12:32:12 +0800

    save videos

--- App/tools/camerautl.py
+++ App/tools/camerautl.py
@@ -2,7 +2,6 @@
 # Date: 27.09.20
 # Desc: This scrtipt script..
 import cv2
-from imutils.video.pivideostream import PiVideoStream
 import imutils
 import time
 import numpy as np
@@ -16,33 +15,109 @@ import threading
 import os
 SAVE_PATH = "/home/pi/Videos/Test"  # will be created if doesnt exist
 
-class Streaming(object):
-    def __init__(self, save_path='', angle=0):
-        self.vs = PiVideoStream(framerate=30).start()
-        self.save_path = save_path
-        self.angle = angle
-        time.sleep(2.0)
+# class Streaming(object):
+#     def __init__(self, save_path='', angle=0):
+#         self.vs = PiVideoStream(framerate=30).start()
+#         self.save_path = save_path
+#         self.angle = angle
+#         time.sleep(2.0)
+#
+#     def __del__(self):
+#         self.vs.stop()
+#
+#     def rotate(self, frame):
+#         return np.rot90(frame, self.angle)
+#
+#     def get_frame(self):
+#         frame = self.rotate(self.vs.read())
+#         ret, jpeg = cv2.imencode('.jpg', frame)
+#         return jpeg.tobytes()
+#
+#     def save_frames(self):
+#         frame = self.rotate(self.vs.read())
+#         ret, jpeg = cv2.imencode('.jpg', frame)
+#         i = 0
+#         path = self.save_path + '{}.jpg'
+#         while i < 10:
+#             cv2.imwrite(path.format(i), frame)
+#             i += 1
+#             time.sleep(0.5)
+#
+# camerautl = Streaming(save_path=SAVE_PATH, angle=2)  # flip pi camera if upside down.
+
+class RecordingThread(threading.Thread):
+    def __init__(self, name, camera):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.isRunning = True
+
+        self.cap = camera
+        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+        self.out = cv2.VideoWriter('./static/video.avi', fourcc, 20.0, (640, 480))
+
+    def run(self):
+        while self.isRunning:
+            ret, frame = self.cap.read()
+            if ret:
+                self.out.write(frame)
+
+        self.out.release()
+
+    def stop(self):
+        self.isRunning = False
 
     def __del__(self):
-        self.vs.stop()
+        self.out.release()
 
-    def rotate(self, frame):
-        return np.rot90(frame, self.angle)
+
+class VideoCamera(object):
+    def __init__(self):
+        # 打开摄像头， 0代表笔记本内置摄像头
+        self.cap = cv2.VideoCapture(0)
+
+        # 初始化视频录制环境
+        self.is_record = False
+        self.out = None
+
+        # 视频录制线程
+        self.recordingThread = None
+
+    # 退出程序释放摄像头
+    def __del__(self):
+        self.cap.release()
 
     def get_frame(self):
-        frame = self.rotate(self.vs.read())
-        ret, jpeg = cv2.imencode('.jpg', frame)
-        return jpeg.tobytes()
+        ret, frame = self.cap.read()
 
-    def save_frames(self):
-        frame = self.rotate(self.vs.read())
-        ret, jpeg = cv2.imencode('.jpg', frame)
-        i = 0
-        path = self.save_path + '{}.jpg'
-        while i < 10:
-            cv2.imwrite(path.format(i), frame)
-            i += 1
-            time.sleep(0.5)
+        if ret:
+            ret, jpeg = cv2.imencode('.jpg', frame)
 
+            # 视频录制
+            if self.is_record:
+                if self.out == None:
+                    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+                    self.out = cv2.VideoWriter('./static/video.avi', fourcc, 20.0, (640, 480))
 
-camerautl = Streaming(save_path=SAVE_PATH, angle=2)  # flip pi camera if upside down.
+                ret, frame = self.cap.read()
+                if ret:
+                    self.out.write(frame)
+            else:
+                if self.out != None:
+                    self.out.release()
+                    self.out = None
+
+            return jpeg.tobytes()
+
+        else:
+            return None
+
+    def start_record(self):
+        self.is_record = True
+        self.recordingThread = RecordingThread("Video Recording Thread", self.cap)
+        self.recordingThread.start()
+
+    def stop_record(self):
+        self.is_record = False
+
+        if self.recordingThread != None:
+            self.recordingThread.stop()
--- App/views/surveillance.py
+++ App/views/surveillance.py
@@ -1,31 +1,60 @@
 from flask import Flask, Blueprint, render_template, session, redirect, url_for, request, Response
 from App.models import db, Order
+
 surveillanceblue = Blueprint('surveillanceblue', __name__)
+camerautl = None
+global_frame = None
+SAVE_PATH = "/home/pi/Videos/Test"  # will be created if doesnt exist
 if True:
-    from App.tools.camerautl import camerautl
+    from App.tools.camerautl import VideoCamera
 else:
-    from App.toolsvirtual.camerautl import camerautl
+    from App.toolsvirtual.camerautl import VideoCamera
+
 
 @surveillanceblue.route('/surveillance/camera', methods=['POST', 'GET'])
 def camera():
     return render_template('surveillance/camera.html')
 
 
-def gen(camerautl):
-    # get camera frame
+
+
+def gen():
+    global camerautl
+    global global_frame
+
+    if camerautl is None:
+        camerautl = VideoCamera()
+
     while True:
         frame = camerautl.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+        if frame is not None:
+            global_frame = frame
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        else:
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + global_frame + b'\r\n\r\n')
 
 
 @surveillanceblue.route('/surveillance/streaming')
 def video_feed():
-    return Response(gen(camerautl),
+    return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-@surveillanceblue.route('/surveillance/savevideo')
-def save_video():
-    camerautl.save_frames()
-    return 'save'
+@surveillanceblue.route('/surveillance/recordstatus', methods=['POST'])
+def record_status():
+    if camerautl is None:
+        video_camera = VideoCamera()
+
+    json = request.get_json()
+
+    status = json['status']
+
+    if status == "true":
+        video_camera.start_record()
+        return jsonify(result="started")
+    else:
+        video_camera.stop_record()
+        return jsonify(result="stopped")
