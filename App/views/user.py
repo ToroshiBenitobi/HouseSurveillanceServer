50ee91dd63cc055d57fb4eb8b4b6ed35d32fcccc
Anarhyroch <Anarhyroch@gmail.com>
2021-04-27 18:48:24 +0800

    save videos

--- App/views/user.py
+++ App/views/user.py
@@ -12,7 +12,7 @@ userblue = Blueprint('userblue', __name__)
 
 # 配置路由
 
-@userblue.route('/', methods=['GET', 'POST'])
+@userblue.route('/123', methods=['POST', 'GET'])
 def login():
     print(1)
     if request.method == 'GET':
