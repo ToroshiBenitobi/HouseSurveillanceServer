e60cd53a6024caf9cb1810f4a39fbaf2990b9cd0
Anarhyroch <Anarhyroch@gmail.com>
2021-04-29 19:52:30 +0800

    save videos

--- App/views/sensor.py
+++ App/views/sensor.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, render_template, session, redirect, url_for, request, Response
+from flask import Flask, Blueprint, render_template, session, redirect, url_for, request, Response, jsonify
 from App.models import db, Order
 if True:
     from App.tools.sensorutl import sensorutl
@@ -8,9 +8,9 @@ sensorblue = Blueprint('sensorblue', __name__)
 
 @sensorblue.route('/sensor/temperatureinfo', methods=['POST', 'GET'])
 def temperatureinfo():
-    return str(sensorutl.temperature())
+    return jsonify(value=str(sensorutl.temperature()))
 
 @sensorblue.route('/sensor/humidityinfo', methods=['POST', 'GET'])
 def humidityinfo():
-    return str(sensorutl.humidity())
+    return jsonify(value=str(sensorutl.humidity()))  
 
