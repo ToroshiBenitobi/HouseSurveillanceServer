268854086c9b628cc3fb1298aef352c332f41c16
Anarhyroch <Anarhyroch@gmail.com>
2021-05-06 20:39:41 +0800

    Revert "save videos"
    This reverts commit 0380f9a7bb7a6021d76dcc71c3193d9fa4c70054.
    
    # Conflicts:
    #	App/tools/scheduler.py

--- App/tools/sensorcheck.py
+++ App/tools/sensorcheck.py
@@ -1,6 +1,12 @@
 from App.tools.sensorutl import sensorutl
-from App.views.surveillance import record_status_without_json
+from App.views.surveillance import camerautl
 from App.tools.scheduler import scheduler
+from App.tools.camerautl import VideoCamera
+from flask import Flask, Blueprint, render_template, session, redirect, url_for, request, Response, jsonify
+from App.models import db, Order
+import json
+import datetime
+from App.models import db, User, Video
 abnormally = False
 TEM_LIMIT = 28
 HUM_LIMIT = 60
@@ -10,17 +16,45 @@ def check_sensor():
         temperature = sensorutl.temperature()
         humidity = sensorutl.humidity()
         print(temperature, humidity)
-        if temperature < TEM_LIMIT and humidity < HUM_LIMIT and not abnormally:
-            print(1)
-            pass
-        elif (temperature >= TEM_LIMIT or humidity >= HUM_LIMIT) and not abnormally:
-            print(2)
+        if not abnormally:
             record_status_without_json(status=True)
-            abnormally = True
-        elif temperature < TEM_LIMIT and humidity < HUM_LIMIT and abnormally:
-            print(3)
+        else:
             record_status_without_json(status=False)
-            abnormally = False
-        elif (temperature >= TEM_LIMIT or humidity >= HUM_LIMIT) and abnormally:
-            print(4)
-            pass
+        # if temperature < TEM_LIMIT and humidity < HUM_LIMIT and not abnormally:
+        #     print(1)
+        #     pass
+        # elif (temperature >= TEM_LIMIT or humidity >= HUM_LIMIT) and not abnormally:
+        #     print(2)
+        #     record_status_without_json(status=True)
+        #     abnormally = True
+        # elif temperature < TEM_LIMIT and humidity < HUM_LIMIT and abnormally:
+        #     print(3)
+        #     record_status_without_json(status=False)
+        #     abnormally = False
+        # elif (temperature >= TEM_LIMIT or humidity >= HUM_LIMIT) and abnormally:
+        #     print(4)
+        #     pass
+
+
+
+def record_status_without_json(status):
+    global camerautl
+    if camerautl is None:
+        camerautl = VideoCamera()
+
+    if status:
+        camerautl.start_record()
+        return True
+    else:
+        save_path = camerautl.stop_record()
+        item = session.get('user')
+        video = Video()
+        video.videoid = video.query.count() + 1
+        video.videoname = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+        video.savepath = save_path
+        if item is not None:
+            userid = item.get('id')
+            video.userid = userid
+        db.session.add(video)
+        db.session.commit()
+        return False
