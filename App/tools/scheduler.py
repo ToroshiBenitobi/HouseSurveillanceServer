557aecd93ed4c9b0c5679ce21dac586f8536c9c4
Anarhyroch <Anarhyroch@gmail.com>
2021-05-06 19:30:32 +0800

    save videos

--- App/tools/scheduler.py
+++ App/tools/scheduler.py
@@ -10,12 +10,15 @@ def check_sensor():
     temperature = sensorutl.temperature()
     humidity = sensorutl.humidity()
     print(temperature, humidity)
-    global abnormally
     if temperature < 27 and humidity < 60 and not abnormally:
         pass
     elif (temperature >= 27 or humidity >= 60) and not abnormally:
         record_status_without_json(status=True)
+        global abnormally
+        abnormally = True
     elif temperature < 27 and humidity < 60 and abnormally:
         record_status_without_json(status=False)
+        global abnormally
+        abnormally = False
     elif (temperature >= 27 or humidity >= 60) and abnormally:
         pass
