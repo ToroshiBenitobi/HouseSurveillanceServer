456ce60c690d868f9695c358d9f0871695dc3117
Anarhyroch <Anarhyroch@gmail.com>
2021-05-07 20:56:38 +0800

    sensorcheck.py

--- App/tools/sensorcheck.py
+++ App/tools/sensorcheck.py
@@ -7,44 +7,52 @@ from App.models import db, Order
 import json
 import datetime
 from App.models import db, User, Video
-abnormally = False
-AUTO_ABNORMALLY_RESET = True
-TEM_LOW_LIMIT = 10
-TEM_HIGH_LIMIT = 30
-HUM_LOW_LIMIT = 10
-HUM_HIGH_LIMIT = 60
-ENTER_NOT_ALLOWED = True
+
+
+class SensorCheckSetting:
+    def __init__(self):
+        self.abnormally = False
+        self.AUTO_ABNORMALLY_RESET = True
+        self.TEM_LOW_LIMIT = 10
+        self.TEM_HIGH_LIMIT = 30
+        self.HUM_LOW_LIMIT = 10
+        self.HUM_HIGH_LIMIT = 60
+        self.ENTER_NOT_ALLOWED = True
+
+
+sensor_check_setting = SensorCheckSetting()
+
+
 def check_sensor():
     with scheduler.app.app_context():
-        global abnormally
+        global sensor_check_setting
         temperature = sensorutl.temperature()
         humidity = sensorutl.humidity()
         is_detected = sensorutl.is_detected()
         print('tem:', temperature, ' hum:', humidity, ' detected:', is_detected)
-        if not abnormally:
+        if not sensor_check_setting.abnormally:
             msg = ''
-            if temperature > TEM_HIGH_LIMIT:
+            if temperature > sensor_check_setting.TEM_HIGH_LIMIT:
                 msg += '温度太高、'
-            if temperature < TEM_LOW_LIMIT:
+            if temperature < sensor_check_setting.TEM_LOW_LIMIT:
                 msg += '温度太低、'
-            if humidity > HUM_HIGH_LIMIT:
+            if humidity > sensor_check_setting.HUM_HIGH_LIMIT:
                 msg += '湿度太高、'
-            if humidity < HUM_LOW_LIMIT:
+            if humidity < sensor_check_setting.HUM_LOW_LIMIT:
                 msg += '湿度太高、'
-            if is_detected and ENTER_NOT_ALLOWED:
+            if is_detected and sensor_check_setting.ENTER_NOT_ALLOWED:
                 msg += '探测到陌生人闯入、'
             if len(msg) > 0:
                 msg = '【宿舍智能管理系统】警告，宿舍检测到如下异常：\n' + msg[:-1] + '。\n请立即登录系统查看情况。'
-                abnormally = True
+                sensor_check_setting.abnormally = True
                 print(msg)
-
 
 
 def reset():
     with scheduler.app.app_context():
-        global abnormally
-        if AUTO_ABNORMALLY_RESET:
-            abnormally = False
+        global sensor_check_setting
+        if sensor_check_setting.AUTO_ABNORMALLY_RESET:
+            sensor_check_setting.abnormally = False
         # if temperature < TEM_LIMIT and humidity < HUM_LIMIT and not abnormally:
         #     print(1)
         #     pass
@@ -59,7 +67,6 @@ def reset():
         # elif (temperature >= TEM_LIMIT or humidity >= HUM_LIMIT) and abnormally:
         #     print(4)
         #     pass
-
 
 
 def record_status_without_json(status):
