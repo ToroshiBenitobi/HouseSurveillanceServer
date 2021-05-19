5e7ecc1ee5df3fa85271110a2b34bb768fa9bef2
Anarhyroch <Anarhyroch@gmail.com>
2021-05-19 20:03:01 +0800

    css

--- App/static/js/updateroominfo.js
+++ App/static/js/updateroominfo.js
@@ -1,4 +1,32 @@
 $(function () {
+     var intemperature = $('#intemperature');
+        var inhumidity = $('#inhumidity');
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            contentType: "application/json",
+            url: "/sensor/temperatureinfo" ,
+            // data: data,
+            success: function (data, status) {
+                intemperature.text(data.value + '℃');
+            },
+            error : function() {
+                intemperature.text('FAILED');
+            }
+        });
+         $.ajax({
+            type: "GET",
+            dataType: "json",
+            contentType: "application/json",
+            url: "/sensor/humidityinfo" ,
+            // data: data,
+            success: function (data, status) {
+                inhumidity.text(data.value + '%');
+            },
+            error : function() {
+                inhumidity.text('FAILED');
+            }
+        });
     $('#updateinfobtn').bind("click", function () {
         var intemperature = $('#intemperature');
         var inhumidity = $('#inhumidity');
